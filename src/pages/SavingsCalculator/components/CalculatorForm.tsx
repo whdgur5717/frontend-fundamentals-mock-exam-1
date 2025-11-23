a7af76963dc4b29aed853ed4b34ea56b654da1c5
whdgur5717 <whdgur5717@gmail.com>
2025-11-23 18:57:27 +0900

    form 로직을 CalculatorForm 컴포넌트로 분리

---
+++ src/pages/SavingsCalculator/components/CalculatorForm.tsx
@@ -0,0 +1,44 @@
+import { SelectBottomSheet, Spacing, TextField } from 'tosslib';
+import type { CalculationFormState } from '../type';
+import type { Dispatch, SetStateAction } from 'react';
+
+export function CalculatorForm({
+  calculationFormState,
+  setCalculationFormState,
+}: {
+  calculationFormState: CalculationFormState;
+  setCalculationFormState: Dispatch<SetStateAction<CalculationFormState>>;
+}) {
+  return (
+    <>
+      <TextField
+        label="목표 금액"
+        placeholder="목표 금액을 입력하세요"
+        suffix="원"
+        value={String(calculationFormState.targetAmount)}
+        onChange={e => setCalculationFormState({ ...calculationFormState, targetAmount: e.target.value })}
+      />
+      <Spacing size={16} />
+      <TextField
+        label="월 납입액"
+        placeholder="희망 월 납입액을 입력하세요"
+        suffix="원"
+        value={String(calculationFormState.monthlyAmount)}
+        onChange={e => setCalculationFormState({ ...calculationFormState, monthlyAmount: e.target.value })}
+      />
+      <Spacing size={16} />
+      <SelectBottomSheet
+        label="저축 기간"
+        title="저축 기간을 선택해주세요"
+        value={calculationFormState.availableTerms}
+        onChange={e => {
+          setCalculationFormState({ ...calculationFormState, availableTerms: e });
+        }}
+      >
+        <SelectBottomSheet.Option value={'6'}>6개월</SelectBottomSheet.Option>
+        <SelectBottomSheet.Option value={'12'}>12개월</SelectBottomSheet.Option>
+        <SelectBottomSheet.Option value={'24'}>24개월</SelectBottomSheet.Option>
+      </SelectBottomSheet>
+    </>
+  );
+}
--- src/pages/SavingsCalculator/index.tsx
+++ src/pages/SavingsCalculator/index.tsx
@@ -2,7 +2,19 @@ import type { CalculationFormState } from './type';
 import { useSuspenseQuery } from '@tanstack/react-query';
 import { getSavingsProuectsQuery, type SavingsProduct } from 'entities/savings/api';
 import { useState } from 'react';
-import { Assets, Border, colors, ListRow, NavigationBar, SelectBottomSheet, Spacing, Tab, TextField } from 'tosslib';
+import {
+  Assets,
+  Border,
+  colors,
+  ListHeader,
+  ListRow,
+  NavigationBar,
+  SelectBottomSheet,
+  Spacing,
+  Tab,
+  TextField,
+} from 'tosslib';
+import { CalculatorForm } from './components/CalculatorForm';
 
 export function SavingsCalculatorPage() {
   const [calculationFormState, setCalculationFormState] = useState<CalculationFormState>({
@@ -25,6 +37,18 @@ export function SavingsCalculatorPage() {
   });
 
   const [selectedProductId, setSelectedProductId] = useState<string | null>(null);
+  const selectedProduct = savingsProducts.find(product => product?.id === selectedProductId);
+
+  // const 예상_수익_금액 =
+  //   Number(calculationFormState.monthlyAmount) *
+  //   Number(calculationFormState.targetAmount) *
+  //   (1 + selectedProduct?.annualRate * 0.5);
+
+  // const 목표_금액과의_차이 = Number(calculationFormState.targetAmount) - 예상_수익_금액;
+
+  // const 추천_월_납입_금액 =
+  //   Number(calculationFormState.targetAmount) /
+  //   (Number(calculationFormState.availableTerms) * (1 + selectedProduct?.annualRate * 0.5));
 
   return (
     <>
@@ -32,143 +56,13 @@ export function SavingsCalculatorPage() {
 
       <Spacing size={16} />
 
-      <TextField
-        label="목표 금액"
-        placeholder="목표 금액을 입력하세요"
-        suffix="원"
-        value={String(calculationFormState.targetAmount)}
-        onChange={e => setCalculationFormState({ ...calculationFormState, targetAmount: e.target.value })}
-      />
-      <Spacing size={16} />
-      <TextField
-        label="월 납입액"
-        placeholder="희망 월 납입액을 입력하세요"
-        suffix="원"
-        value={String(calculationFormState.monthlyAmount)}
-        onChange={e => setCalculationFormState({ ...calculationFormState, monthlyAmount: e.target.value })}
-      />
-      <Spacing size={16} />
-      <SelectBottomSheet
-        label="저축 기간"
-        title="저축 기간을 선택해주세요"
-        value={calculationFormState.availableTerms}
-        onChange={e => {
-          setCalculationFormState({ ...calculationFormState, availableTerms: e });
-        }}
-      >
-        <SelectBottomSheet.Option value={'6'}>6개월</SelectBottomSheet.Option>
-        <SelectBottomSheet.Option value={'12'}>12개월</SelectBottomSheet.Option>
-        <SelectBottomSheet.Option value={'24'}>24개월</SelectBottomSheet.Option>
-      </SelectBottomSheet>
+      <CalculatorForm calculationFormState={calculationFormState} setCalculationFormState={setCalculationFormState} />
 
       <Spacing size={24} />
       <Border height={16} />
       <Spacing size={8} />
 
-      <Tab onChange={() => {}}>
-        <Tab.Item value="products" selected={true}>
-          적금 상품
-        </Tab.Item>
-        <Tab.Item value="results" selected={false}>
-          계산 결과
-        </Tab.Item>
-      </Tab>
-
-      {savingsProducts?.map(product => (
-        <ListRow
-          key={product.id}
-          contents={
-            <ListRow.Texts
-              type="3RowTypeA"
-              top={product.name}
-              topProps={{ fontSize: 16, fontWeight: 'bold', color: colors.grey900 }}
-              middle={`연 이자율: ${product.annualRate}%`}
-              middleProps={{ fontSize: 14, color: colors.blue600, fontWeight: 'medium' }}
-              bottom={''}
-              bottomProps={{ fontSize: 13, color: colors.grey600 }}
-            />
-          }
-          right={selectedProductId === product.id ? <Assets.Icon name="icon-check-circle-green" /> : undefined}
-          onClick={() => {
-            setSelectedProductId(product.id);
-          }}
-        />
-      ))}
-
-      {/* 아래는 계산 결과 탭 내용이에요. 계산 결과 탭을 구현할 때 주석을 해제해주세요. */}
-      {/* <Spacing size={8} />
-
-      <ListRow
-        contents={
-          <ListRow.Texts
-            type="2RowTypeA"
-            top="예상 수익 금액"
-            topProps={{ color: colors.grey600 }}
-            bottom={`1,000,000원`}
-            bottomProps={{ fontWeight: 'bold', color: colors.blue600 }}
-          />
-        }
-      />
-      <ListRow
-        contents={
-          <ListRow.Texts
-            type="2RowTypeA"
-            top="목표 금액과의 차이"
-            topProps={{ color: colors.grey600 }}
-            bottom={`-500,000원`}
-            bottomProps={{ fontWeight: 'bold', color: colors.blue600 }}
-          />
-        }
-      />
-      <ListRow
-        contents={
-          <ListRow.Texts
-            type="2RowTypeA"
-            top="추천 월 납입 금액"
-            topProps={{ color: colors.grey600 }}
-            bottom={`100,000원`}
-            bottomProps={{ fontWeight: 'bold', color: colors.blue600 }}
-          />
-        }
-      />
-
-      <Spacing size={8} />
-      <Border height={16} />
-      <Spacing size={8} />
-
-      <ListHeader title={<ListHeader.TitleParagraph fontWeight="bold">추천 상품 목록</ListHeader.TitleParagraph>} />
-      <Spacing size={12} />
-
-      <ListRow
-        contents={
-          <ListRow.Texts
-            type="3RowTypeA"
-            top={'기본 정기적금'}
-            topProps={{ fontSize: 16, fontWeight: 'bold', color: colors.grey900 }}
-            middle={`연 이자율: 3.2%`}
-            middleProps={{ fontSize: 14, color: colors.blue600, fontWeight: 'medium' }}
-            bottom={`100,000원 ~ 500,000원 | 12개월`}
-            bottomProps={{ fontSize: 13, color: colors.grey600 }}
-          />
-        }
-        onClick={() => {}}
-      />
-      <ListRow
-        contents={
-          <ListRow.Texts
-            type="3RowTypeA"
-            top={'고급 정기적금'}
-            topProps={{ fontSize: 16, fontWeight: 'bold', color: colors.grey900 }}
-            middle={`연 이자율: 2.8%`}
-            middleProps={{ fontSize: 14, color: colors.blue600, fontWeight: 'medium' }}
-            bottom={`50,000원 ~ 1,000,000원 | 24개월`}
-            bottomProps={{ fontSize: 13, color: colors.grey600 }}
-          />
-        }
-        onClick={() => {}}
-      />
-
-      <Spacing size={40} /> */}
+      <Spacing size={40} />
 
       {/* 아래는 사용자가 적금 상품을 선택하지 않고 계산 결과 탭을 선택했을 때 출력해주세요. */}
       {/* <ListRow contents={<ListRow.Texts type="1RowTypeA" top="상품을 선택해주세요." />} /> */}
