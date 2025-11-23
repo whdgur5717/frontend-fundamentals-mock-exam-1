d90090c0ca7f09847e68b64ca815220fb42689ae
whdgur5717 <whdgur5717@gmail.com>
2025-11-23 15:30:28 +0900

    tanstack query 설치 및 provider 등록

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import { GlobalPortal, GlobalStyles } from 'tosslib';
 import { Routes } from './pages/Routes';
+import { QueryClientProvider } from '@tanstack/react-query';
+import { queryClient } from './libs/queryClient';
 
 export function App() {
   return (
     <>
       <GlobalStyles />
       <GlobalPortal.Provider>
-        <Routes />
+        <QueryClientProvider client={queryClient}>
+          <Routes />
+        </QueryClientProvider>
       </GlobalPortal.Provider>
     </>
   );
